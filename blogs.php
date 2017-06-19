7240c15cedbab56030442466b3d786a55384dbd2
jahoffgir <amjahongir@gmail.com>
2017-06-18 23:57:21 -0400

    Added all the files

---
+++ blogs.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+		<!-- The head of the page -->
+		 <head> <title>Blogs</title>
+		 	<meta charset="utf-8" />
+		 	<link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+			<link rel="shortcut icon" href="Images/fl.png">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+		 	</head>
+		 	<!-- Body of the page -->
+		 	<body>
+		 		<!-- Navigation of the page -->
+				<?php
+		    include 'assets/php/nav.php';
+		    ?>
+		    <img src="assets/images/icons/arrow.jpg" alt="">
+	 		
+			<!-- Footer -->
+			<?php
+	    	include 'assets/php/footer.php';
+	    	?>
+		 	</body>
+</html>
