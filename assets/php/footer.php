63ad9f5eb91e2960a2c6c524609ddfc6b3e473c5
Jahongir A <amjahongir@gmail.com>
2017-09-01 18:25:05 -0400

    Added new files

---
+++ assets/php/footer.php
@@ -0,0 +1,9 @@
+<?php
+// footer for the site
+echo <<<END
+<footer>
+  <a class="top" href="#top">Go to top</a>
+  <p>All rights reserved &copy; 2016<p>
+</footer>
+END;
+?>
---
+++ experiences.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+		<!-- The head of the page -->
+		 <head> <title>Experiences</title>
+		 	<meta charset="utf-8" />
+		 	<link rel="stylesheet"href="assets/css/custom.css">
+			<link rel="shortcut icon" href="Images/fl.png">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+		 	</head>
+		 	<!-- Body of the page -->
+		 	<body>
+		 		<!-- Navigation of the page -->
+				<?php
+		    include 'assets/php/nav.php';
+		    ?>
+	 		
+			<!-- Footer -->
+			<?php
+	    	include 'assets/php/footer.php';
+	    	?>
+		 	</body>
+</html>
